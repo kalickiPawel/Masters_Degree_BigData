de664df206e008962f0f05ec9f077f0e2bcef6ca
bartek <bmalachowski@zut.edu.pl>
2019-11-22 09:27:00 +0100

    demo

--- src/main/java/sparkdemo/SparkDemo.java
+++ src/main/java/sparkdemo/SparkDemo.java
@@ -12,21 +12,14 @@ import scala.Tuple2;
 public class SparkDemo {
 
         public static void main(String[] args) {
-                // Create a SparkContext to initialize
                 SparkConf conf = new SparkConf().setMaster("local").setAppName("Word Count");
-
-                // Create a Java version of the Spark Context
                 JavaSparkContext sc = new JavaSparkContext(conf);
-
-                // Load the text into a Spark RDD, which is a distributed representation of each
-                // line of text
-                // JavaRDD<String> textFile = sc.textFile("src/main/resources/shakespeare.txt");
-                JavaRDD<String> textFile = sc.textFile("hdfs:///tmp/shakespeare.txt");
+                JavaRDD<String> textFile = sc.textFile("src/main/resources/shakespeare.txt");
                 JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
                                 .mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
                 counts.foreach(p -> System.out.println(p));
                 System.out.println("Total words: " + counts.count());
-                counts.saveAsTextFile("hdfs:///tmp/shakespeareWordCount");
+                counts.saveAsTextFile("target/tmp/shakespeareWordCount");
         }
 
 }
