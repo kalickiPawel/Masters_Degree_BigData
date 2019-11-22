8946521c9d4b5e309f5a836a7d3e63a56c9abdb0
Bartek Małachowski <bartek.malachowski@gmail.com>
2019-11-22 05:32:36 +0100

    example

--- src/main/java/sparkdemo/SparkDemo.java
+++ src/main/java/sparkdemo/SparkDemo.java
@@ -20,13 +20,13 @@ public class SparkDemo {
 
                 // Load the text into a Spark RDD, which is a distributed representation of each
                 // line of text
-                JavaRDD<String> textFile = sc.textFile("src/main/resources/shakespeare.txt");
+                // JavaRDD<String> textFile = sc.textFile("src/main/resources/shakespeare.txt");
+                JavaRDD<String> textFile = sc.textFile("hdfs:///tmp/shakespeare.txt");
                 JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
                                 .mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
                 counts.foreach(p -> System.out.println(p));
                 System.out.println("Total words: " + counts.count());
-                counts.saveAsTextFile("/tmp/shakespeareWordCount");
-
+                counts.saveAsTextFile("hdfs:///tmp/shakespeareWordCount");
         }
 
 }
