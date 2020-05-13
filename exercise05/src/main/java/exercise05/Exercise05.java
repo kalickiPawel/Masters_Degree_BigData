a8eff9fd9b068721298b0ae3ab5673d031e9bdbb
Paweł Kalicki <kalicki.paul@icloud.com>
2020-05-13 16:22:34 +0200

    Added count ratings more than 10

--- exercise05/src/main/java/exercise05/Exercise05.java
+++ exercise05/src/main/java/exercise05/Exercise05.java
@@ -9,6 +9,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import org.apache.spark.mllib.recommendation.ALS;
 import org.apache.spark.mllib.recommendation.Rating;
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
 
 import scala.Tuple2;
 
@@ -23,12 +24,24 @@ public class Exercise05 {
 		JavaRDD<String> ratings = sc.textFile("../data/ratings.csv");
 		JavaRDD<String> movies = sc.textFile("../data/movies.csv");
 
+		JavaPairRDD<String, Integer> ratingsCount = movies.map(linia -> linia.split(","))
+				.mapToPair(tablica -> new Tuple2<>(tablica[1], 1)).reduceByKey((a, b) -> a + b)
+				.mapToPair(tablica -> new Tuple2<>(tablica._1, tablica._2)).filter(x -> x._2 > 10);
+
+		ratingsCount.foreach(el -> System.out.println(el));
+
 		JavaRDD<Rating> tr = ratings.map(line -> {
 			String[] arr = line.split(",");
-			return new Rating(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Double.parseDouble(arr[2]));
+			return new Rating(Integer.parseInt(arr[1]), Integer.parseInt(arr[0]), Double.parseDouble(arr[2]));
 		});
 
-		ALS.train(tr.rdd(), 10, 10);
+		MatrixFactorizationModel model = ALS.train(tr.rdd(), 10, 10);
+
+		Rating[] rec = model.recommendProducts(1, 3);
+
+		for (Rating r : rec) {
+			System.out.println(r);
+		}
 
 		sc.stop();
 		sc.close();
