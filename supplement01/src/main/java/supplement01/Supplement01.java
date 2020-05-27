5be2c11de004d9ca050fab73ca68771e2cc2299b
Paweł Kalicki <kalicki.paul@icloud.com>
2020-05-27 15:09:32 +0200

    Added dependencies

--- supplement01/src/main/java/supplement01/Supplement01.java
+++ supplement01/src/main/java/supplement01/Supplement01.java
@@ -7,18 +7,20 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Supplement01 {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("Word Count");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        // JavaRDD<String> textFile =
-        // sc.textFile("file:///home/maria_dev/shakespeare.txt"); // ścieżka bezwzględna
-        JavaRDD<String> textFile = sc.textFile("data/shakespeare.txt"); // ścieżka względna
-        JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
-                .mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
-        counts.foreach(p -> System.out.println(p));
-        System.out.println("Total words: " + counts.count());
+
+        List<MyEdge> edgeList = Arrays.asList(
+                new MyEdge(1L,2L,"a"), new MyEdge(1L,3L,"a"),
+                new MyEdge(1L,4L,"a"), new MyEdge(2L,3L,"b"),
+                new MyEdge(3L,4L,"a"), new MyEdge(2L,4L,"b")
+        );
+
+
         sc.stop();
         sc.close();
     }
